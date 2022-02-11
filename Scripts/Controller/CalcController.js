dbaa7be015d2167990a4d2b0cfabd5dec0420f9a
luan-evangelista <luan.evieira43@gmail.com>
2022-02-11 02:54:55 -0300

    Add: Método

--- Scripts/Controller/CalcController.js
+++ Scripts/Controller/CalcController.js
@@ -4,6 +4,19 @@ class CalcController {
 
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "11/02/2022";
+        timeEl.innerHTML = "01:59";
 
     }
 
